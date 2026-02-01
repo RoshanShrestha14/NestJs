1e9c1643a13ff50bb52fd7299d28867eaabd8c9f
Roshan Shrestha <roshanshresthawd@gmail.com>
2026-02-01 22:48:49 +0545

    controllers

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ProductService } from './product.service';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+
+    constructor(private readonly productsService: ProductService){}
+    @Get()
+    getAllProducts() {
+          return this.productsService.getAllProducts();
+        // Logic to get all products
+    }
+    @Get(':id')
+    getProductById(@Param('id') id: string) {
+      return this.productsService.getProductById(Number(id));
+    }
+}
