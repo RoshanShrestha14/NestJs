504ba9616a69136c58952b833ab3b0fe4286a38f
Roshan Shrestha <roshanshresthawd@gmail.com>
2026-02-03 15:34:29 +0545

    perform crud operation on services

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -13,7 +13,7 @@ export class StudentService {
   }
 
   getStudentById(id: number) {
-    const student = this.students.find(stu => stu.id === id);
+    const student = this.students.find((stu) => stu.id === id);
     if (!student) {
       throw new NotFoundException(`Student with id ${id} not found`);
     }
@@ -28,4 +28,70 @@ export class StudentService {
     this.students.push(newStudent);
     return newStudent;
   }
+  //put
+  updateStudent(id: number, data: { name: string; age: number }) {
+    const existingStudent = this.students.find((student) => student.id === id);
+
+    if (!existingStudent) {
+      throw new NotFoundException('No student found');
+    }
+
+    existingStudent.name = data.name;
+    existingStudent.age = data.age;
+
+    return {
+      message: 'updated',
+      success: true,
+      student: existingStudent,
+    };
+  }
+
+// updateStudent(id: number, data: { name: string; age: number }) {
+//   const index = this.students.findIndex(
+//     student => student.id === id
+//   );
+
+//   if (index === -1) {
+//     throw new NotFoundException("No student found");
+//   }
+
+//   this.students[index] = {
+//     ...this.students[index],
+//     ...data,
+//   };
+
+//   return {
+//     message: "updated",
+//     success: true,
+//     student: this.students[index],
+//   };
+// }
+
+
+patchStudent(id:number,data:Partial<{name:string,age:number}>)
+{
+    const student= this.getStudentById(id);
+    Object.assign(student,data);
+     return {
+      message: 'patched',
+      success: true,
+      student: student
+    };
+}
+
+deleteStudent(id:number){
+
+    const index =  this.students.findIndex((s)=>s.id=== id);
+    if(index===-1 )  throw new NotFoundException("id not found");
+
+const deleted  = this.students.splice(index,1)
+return {
+      message: 'deleted',
+      success: true,
+      student: deleted
+    };
+
+
+
+}
 }
