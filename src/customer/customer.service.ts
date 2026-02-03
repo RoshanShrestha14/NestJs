6a937b41aeda778d7cf088e35ccaf3b4452f1bee
Roshan Shrestha <roshanshresthawd@gmail.com>
2026-02-03 18:49:38 +0545

    used dto and interfaces

--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { Customer } from './interfaces/customer.interface';
+import { CreateCustomerDto } from './dto/create-customer.dto';
 
 @Injectable()
-export class CustomerService {}
+export class CustomerService {
+  private customers: Customer[] = [];
+
+  getAllCustomer(): Customer[] {
+    return this.customers;
+  }
+
+  addCustomer(CreateCustomerDto: CreateCustomerDto): Customer {
+    const newCustomer: Customer = {
+      id: this.customers.length + 1,
+      ...CreateCustomerDto,
+    };
+    this.customers.push(newCustomer);
+    return newCustomer;
+  }
+}
