09a4c53005d8e75215cec7eec435a8301c59a2e7
Roshan Shrestha <roshanshresthawd@gmail.com>
2026-02-08 14:46:04 +0545

    performed Crud operation

--- src/student-s/student-s.controller.ts
+++ src/student-s/student-s.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { StudentSService } from './student-s.service';
 import { Student } from './student.schema';
 
@@ -6,8 +6,31 @@ import { Student } from './student.schema';
 export class StudentSController {
   constructor(private readonly studentService: StudentSService) {}
 
+  @Get()
+  async getAllStudent() {
+    return this.studentService.getAllStudent();
+  }
+
   @Post()
   async addStudent(@Body() data: Partial<Student>) {
     return this.studentService.createStudent(data);
+  }
+  @Get(':id')
+  async getStudentById(@Param('id') id: string) {
+    console.log('CONTROLLER HIT, id =', id);
+
+    return this.studentService.getStudentById(id);
+  }
+
+  @Put(':id')
+  async upadteStudent(@Param('id') id: string, @Body() data: Partial<Student>) {
+    return this.studentService.updateStudent(id, data);
+  }
+
+  @Delete(":id")
+  async deleteStudent (@Param("id") id:string)
+  { 
+    console.log("id deleted")
+    return this.studentService.deleteStudentById(id);
   }
 }
--- src/student-s/student-s.service.ts
+++ src/student-s/student-s.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, Param } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Student, studentDocument } from './student.schema';
 import { Model } from 'mongoose';
@@ -13,5 +13,29 @@ export class StudentSService {
     const newStudent = new this.studentModel(data);
 
     return newStudent.save();
+  }
+
+  async getAllStudent(): Promise<Student[]> {
+    return this.studentModel.find().exec();
+  }
+
+  async getStudentById(id: string): Promise<Student | null> {
+    console.log(`id is ${id}`);
+
+    return this.studentModel.findById(id).exec();
+  }
+
+  async updateStudent(
+    id: string,
+    data: Partial<Student>,
+  ): Promise<Student | null> {
+    return this.studentModel.findByIdAndUpdate(id, data, { new: true }).exec();
+  }
+
+
+
+  async deleteStudentById(id: string): Promise<Student | null> {
+
+    return this.studentModel.findByIdAndDelete(id).exec();
   }
 }
